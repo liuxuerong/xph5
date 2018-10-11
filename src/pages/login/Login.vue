e6d7bc240d58526f97a9f2d6d997fd9941e9d494
betrayalFan <3219699854@qq.com>
2018-10-11 10:33:12 +0800

    登录注册

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,161 @@
+<template>
+    <div class="loginWrapper">
+        <div class="loginTop">
+            <span>欢迎回来</span>
+            <router-link to="/" class="backIndex">回到首页</router-link>
+        </div>
+        <div class="loginInfo">
+            <form id="loginInfoForm">
+                <div class="loginInput userName border-bottom">
+                    <i class="loginIcon"></i>
+                    <input type="text" placeholder="手机号码" name="phone" maxlength="11" v-model="userInfo.username"/>
+                </div>
+                <div class="loginInput passWord">
+                    <i class="loginIcon"></i>
+                    <input type="password" placeholder="密码" name="password" maxlength="12" v-model="userInfo.password"/>
+                    <router-link to="/remberPassword" class="passWordOper">忘记密码？</router-link>
+                </div>
+            </form>
+        </div>
+        <button class="loginBtn" @click="loginBtnClick()">登陆</button>
+        <router-link to="/phoneCode" class="phoneCodeLogin">手机验证码登陆</router-link>
+        <span class="loginTipText">未注册的手机号码验证后自动创建星品账户</span>
+    </div>
+</template>
+
+<script type="text/javascript">
+// const axios = require('axios')
+export default {
+  data () {
+    return {
+      userInfo: {
+        username: 17688758824,
+        password: 123456,
+        type: 1,
+        userType: 1
+      }
+    }
+  },
+  computed: {
+    // 判断手机号码
+    rightPhoneNumber: function () {
+      return /^1\d{10}$/gi.test(this.phoneNumber)
+    }
+  },
+  methods: {
+    loginBtnClick: function () {
+      console.log('点击')
+      console.log(this.rightPhoneNumber)
+    }
+  },
+  mounted: function () {
+
+  }
+//   beforeCreate: function () {
+//     console.group('beforeCreate 创建前状态')
+//   },
+//   created: function () {
+//     console.group('created 创建完毕状态')
+//   }
+//   beforeMount: function () {
+//     console.group('beforeMount 挂载前状态===============》')
+//   },
+//   beforeUpdate: function () {
+//     console.group('beforeUpdate 更新前状态===============》')
+//   },
+//   updated: function () {
+//     console.group('updated 更新完成状态===============》')
+//   },
+// beforeDestroy: function () {
+//     console.group('beforeDestroy 销毁前状态===============》')
+//   },
+// destroyed: function () {
+//     console.group('destroyed 销毁完成状态===============》')
+//   }
+}
+</script>
+
+<style lang="stylus" scoped>
+    body,html
+        height 100%
+        width 100%
+        position relative
+    .loginWrapper
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+        background gray
+        overflow hidden
+        box-sizing border-box
+        padding-top 180px
+    .loginTop
+        height 72px
+        line-height 72px
+        box-sizing border-box
+        padding 0 44px
+        span
+            font-size 76px
+            color #fff
+        .backIndex
+            float right
+            font-size 36px
+            color #fff
+    .loginInfo
+        width 88%
+        height 530px
+        background rgba(0,0,0,0.1)
+        margin 20% auto 5%
+        box-sizing border-box
+        padding 0 30px
+        .loginInput
+            width 100%
+            height 265px
+            line-height 265px
+            .loginIcon
+                display inline-block
+                width 60px
+                height 60px
+                background red
+                margin-right 56px
+                vertical-align middle
+            input
+                width 62%
+                height 100px
+                background transparent
+                color #fff
+                outline none
+            .passWordOper
+                float right
+                color #fff
+                font-size 36px
+    .loginBtn
+        display block
+        width 88%
+        height 160px
+        line-height 160px
+        text-align center
+        font-size 46px
+        color #fff
+        border-radius 4px
+        background #C78A5C
+        margin 0 auto 5%
+    .phoneCodeLogin
+        display block
+        width 100%
+        text-align center
+        font-size 40px
+        height 40px
+        line-height 42px
+        color #fff
+        text-decoration underline
+    .loginTipText
+        display block
+        width 100%
+        text-align center
+        font-size 36px
+        color #E6E6E6
+        position absolute
+        bottom 8%
+</style>
---
+++ src/pages/login/RemberPassword.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="wrapper">
+        <div class="userTop">
+            <router-link to="">》</router-link>
+            <router-link to="">下一步</router-link>
+        </div>
+        <div class="userContent">
+            <h3>找回密码</h3>
+            <div class="userPhone">
+                <span class="border-bottom">+86</span>
+                <div class="border-bottom phoneInput">
+                    <input type="text" placeholder="手机号码" name="" id="">
+                </div>
+            </div>
+            <div class="userPhoneCode border-bottom">
+                <input type="text" placeholder="验证码" name="" id="">
+                <span>获取验证码</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+    body,html
+        width 100%
+        height 100%
+        position relative
+    .wrapper
+        position absolute
+        left 0
+        top 0
+        width 100%
+        height 100%
+        background #fff
+        box-sizing border-box
+        padding-top 130px
+    .userTop
+        width 100%
+        height 52px
+        line-height 52px
+        background green
+    .userContent
+        width 92%
+        margin 68px auto 0
+        h3
+            width 100%
+            font-size 76px
+            font-weight 600
+            color #262626
+            margin-bottom 46px
+        span
+            display block
+            width 100%
+            font-size 40px
+            color #808080
+    .userPhone
+        width 100%
+        height 150px
+        margin 70px auto 0
+        span
+            display inline-block
+            width 15%
+            height 140px
+            line-height 140px
+            font-size 42px
+            color #808080
+            text-align center
+            margin-right 4%
+            vertical-align bottom
+        .phoneInput
+            display inline-block
+            width 80%
+            background red
+        input
+            width 100%
+            height 140px
+            line-height 140px
+            font-size 40px
+            color #262626
+            border none
+            outline  none
+    .userPhoneCode
+        input
+            width 76%
+            height 140px
+            line-height 140px
+            font-size 40px
+            color #262626
+            border none
+            outline  none
+        span
+            float right
+            width auto
+            line-height 140px
+            font-size 36px
+            color #C78A5C
+            box-sizing border-box
+            padding-right 4%
+</style>
