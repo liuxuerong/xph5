cec929924f32779121ed8dd6d580b0f79fdbeb47
betrayalFan <3219699854@qq.com>
2018-10-12 09:29:00 +0800

    登陆注册

--- src/pages/login/PhoneCode.vue
+++ src/pages/login/PhoneCode.vue
@@ -8,31 +8,108 @@
             <form id="loginInfoForm">
                 <div class="loginInput userName border-bottom">
                     <i class="loginIcon"></i>
-                    <input type="text" placeholder="手机号码" name="" id="" autocomplete="off"/>
+                    <input type="text" placeholder="手机号码" name="" id="" v-model="request"/>
                 </div>
                 <div class="loginInput passWord">
                     <i class="loginIcon"></i>
-                    <input type="text" placeholder="验证码" name="" id="" autocomplete="off"/>
-                    <span class="obtainCode">获取验证码</span>
+                    <input type="text" placeholder="验证码" name="" id="" v-model="code"/>
+                    <span class="obtainCode" @click="obtainCodeBtn" v-show="!computedTime">获取验证码</span>
+                    <span class="obtainCode" v-show="computedTime">已发送 {{computedTime}} s</span>
                 </div>
             </form>
         </div>
-        <button class="loginBtn">登陆</button>
+        <button class="loginBtn" @click="loginPhoneCode">登陆</button>
         <router-link to="/login" class="phoneCodeLogin">账号密码登录</router-link>
         <span class="loginTipText">未注册的手机号码验证后自动创建星品账户</span>
     </div>
 </template>
 
 <script type="text/javascript">
+import {Toast} from 'mint-ui'
 const axios = require('axios')
 export default {
   data () {
     return {
-
+      request: '',
+      computedTime: 0,
+      code: ''
+    }
+  },
+  computed: {
+    // 判断手机号码
+    rightPhoneNumber: function () {
+      return /^1\d{10}$/gi.test(this.request)
+    },
+    // 判断验证码
+    rightPhoneCode: function () {
+      return /^\d{6}$/gi.test(this.code)
     }
   },
   methods: {
+    //   获取验证码
+    obtainCodeBtn: function () {
+      if (this.rightPhoneNumber) {
+        axios.post('https://api.test.jdhoe.com/v1/sms/send/1', {
+          request: this.request
+        }).then((response) => {
+          console.log(response)
+          if (response.data.code === 0) {
+            this.computedTime = 60
+            this.timer = setInterval(() => {
+              this.computedTime--
+              if (this.computedTime === 0) {
+                clearInterval(this.timer)
+              }
+            }, 1000)
+          }
+        }).catch((error) => {
+          console.log(error)
+        })
+      } else {
+        Toast({
+          message: '请输入正确的手机号码',
+          position: 'bottom',
+          duration: 5000
+        })
+      }
+    },
 
+    // 登陆
+    loginPhoneCode: function () {
+      console.log(56666)
+      console.log(this.rightPhoneCode)
+      if (this.rightPhoneNumber && this.rightPhoneCode) {
+        console.log(this.request)
+        console.log(this.code)
+        axios.post('https://api.test.jdhoe.com/auth/token', {
+          phone: this.request,
+          code: this.code,
+          type: 2,
+          userType: 1
+        }).then((response) => {
+          console.log(response)
+          if (response.data.code === 0) {
+            console.log('成功-------------')
+            window.localStorage.setItem('userId', response.data.body.user_id)
+            window.localStorage.setItem('token', response.data.body.access_token)
+          } else if (response.data.code === 12004) {
+            Toast({
+              message: '请输入正确的验证码',
+              position: 'bottom',
+              duration: 5000
+            })
+          }
+        }).catch((error) => {
+          console.log(error)
+        })
+      } else {
+        Toast({
+          message: '请输入正确的手机号码或者验证码',
+          position: 'bottom',
+          duration: 5000
+        })
+      }
+    }
   },
   mounted: function () {
     axios.get('https://api.test.jdhoe.com/article/web/brand/info')
