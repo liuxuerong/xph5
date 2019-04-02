0719a7ea7e0672bbdf930298960454ee5e9f5cf3
kala <827122694@qq.com>
2019-04-02 14:05:01 +0800

    添加注册之后弹窗

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -24,6 +24,60 @@
     <!-- <router-link to="/PhoneCode" class="phoneCodeLogin">账号密码登录</router-link> -->
     <span class="phoneCodeLogin" @click="toggelWay">{{this.innerText}}</span>
     <span class="loginTipText">未注册的手机号码验证后自动创建星品账户</span>
+     <mt-popup position="center" v-model="coupon" @touchmove.prevent class="couponPopup">
+       <div class="couponWrap">
+        <div class="top">
+          <div class="close" @click="coupon=false">×</div>
+          <h2>
+            恭喜你获得
+          </h2>
+          <h4><i>3</i>张优惠券</h4>
+        </div>
+         <div class="content">
+            <ul>
+              <li>
+                <div class="left">
+                  <span class="price"><i>¥</i>30</span>
+                  <span class="tag">满减券</span>
+                </div>
+                <div class="right">
+                  <span class="main">满299减30</span>
+                  <span class="detail">
+                    【通用】全品类通用、不可叠加
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div class="left">
+                  <span class="price"><i>¥</i>20</span>
+                  <span class="tag">满减券</span>
+                </div>
+                <div class="right">
+                  <span class="main">满199减20</span>
+                  <span class="detail">
+                    【通用】全品类通用、不可叠加
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div class="left">
+                  <span class="price"><i>¥</i>50</span>
+                  <span class="tag">满减券</span>
+                </div>
+                <div class="right">
+                  <span class="main">满499减50</span>
+                  <span class="detail">
+                    【通用】全品类通用、不可叠加
+                  </span>
+                </div>
+              </li>
+            </ul>
+           <router-link to="/cardVoucher" class="bottom">
+            查看优惠券
+           </router-link>
+         </div>
+       </div>
+     </mt-popup>
   </div>
 </template>
 
@@ -31,6 +85,7 @@
 import LoginBack from './components/LoginBack'
 import { isPhone, isPhoneCode, rightPassword } from 'util/validate'
 import {
+  Popup,
   Toast
 } from 'mint-ui'
 import {
@@ -57,7 +112,8 @@ export default {
       isCode: true,
       innerText: '账号密码登录',
       username: '',
-      redirect: null
+      redirect: null,
+      coupon: false
     }
   },
   beforeRouteEnter (to, from, next) {
@@ -68,7 +124,8 @@ export default {
     next()
   },
   components: {
-    LoginBack
+    LoginBack,
+    'mt-popup': Popup
   },
 
   methods: {
@@ -280,6 +337,86 @@ export default {
     font-size 36px
     color #E6E6E6
     margin-top 30%
+  .couponWrap
+    width calc(100vw-(160px))
+    height 1297px
+    background url('../../images/coupon_bg.png') no-repeat center center/100% 100%
+    margin 0 auto
+    .top
+      position relative
+      .close
+        position absolute
+        right 38px
+        font-size 100px
+        color #B5B8BB
+        line-height 100px
+    h2
+      color #333333
+      font-size 80px
+      padding 40px 0 0 70px
+    h4
+      line-height 180px
+      color #333333
+      font-size 60px
+      i
+        color #c81623
+        padding-left 70px
+    .content
+      width 100%
+      padding 0 53px
+    ul>li
+      width 100%
+      background-color #fff
+      height 240px
+      margin-bottom 20px
+      display flex
+      .left
+        width 320px
+        .price
+          line-height 157px
+          color #333333
+          font-size 80px
+          display inline-block
+          width 100%
+          text-align center
+        i
+          font-size 40px
+        .tag
+          width 150px
+          line-height 52px
+          background-color #333333
+          color #fff
+          font-size 32px
+          text-align center
+          display block
+          margin 0 auto
+      .right
+        flex 1
+        .main
+          font-size 46px
+          font-weight bold
+          line-height 139px
+          display block
+        .detail
+          font-size 36px
+          color #99A2AA
+    .bottom
+      border 3px solid #333333
+      font-size 46px
+      font-weight bold
+      width 300px
+      height 110px
+      line-height 110px
+      text-align center
+      margin 30px auto 0
+      color #333
+      display block
+  .couponPopup
+    width 100%
+    z-index 999999 !important
+    background-color transparent
+  .v-modal
+    z-index 9999 !important
   ::-webkit-input-placeholder {
     color: #fff;
   }
